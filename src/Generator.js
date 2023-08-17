15e46651bae0fc294dd8555a55e8e8b6a73e4d53
Moosung Kang <bbibbi4808@naver.com>
2023-08-18 03:58:31 +0900

    first commit

---
+++ src/Generator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Generator.css';
+
+function Generator() {
+    return (
+        <div className = "generater">
+
+            <div className='generatorContainer'>
+                <button className='generatorButton'>생성하기</button>
+            </div>
+        </div>
+    );
+}
+
+export default Generator;
